cb05dbb62d13694e6f26c65d02f1a4e7f32cf242
Pradhyumnaa <pradhyumnaag666@gmail.com>
2023-01-21 21:57:46 +0700

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,40 @@
+var router = require('express').Router();
+const { requiresAuth } = require('express-openid-connect');
+
+var userEmail = ""
+
+// router.get('/', function (req, res, next) {
+//   res.render('index', {
+//     title: 'Auth0 Webapp sample Nodejs',
+//     isAuthenticated: req.oidc.isAuthenticated()
+//   });
+// });
+
+router.get('/', function (req, res, next) {
+  if(req.oidc.isAuthenticated()){
+    userEmail = req.oidc.user.email; //This stores the userEmail to a variable. Important because this is the identifier in Database.
+    res.render('subjects', {
+      userProfile: JSON.stringify(req.oidc.user, null, 2)
+    });
+  } else{
+    res.render('loginPage', {
+      isAuthenticated: req.oidc.isAuthenticated()
+    });
+  }
+});
+
+// router.get('/profile', requiresAuth(), function (req, res, next) {
+//   res.render('profile', {
+//     userProfile: JSON.stringify(req.oidc.user, null, 2),
+//     title: 'Index Page'
+//   });
+// });
+
+// router.get('/loginPage', function (req, res, next) {
+//   res.render('loginPage', {
+//     title: 'Auth0 Webapp sample Nodejs',
+//     isAuthenticated: req.oidc.isAuthenticated()
+//   });
+// });
+
+module.exports = router;
